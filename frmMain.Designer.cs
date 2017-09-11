54225d15660de4e1c1a9e916d576bc10b1efef69
broject <boroo_c@yahoo.com>
2017-09-11 23:56:20 +0800

    commit description

---
+++ frmMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MorningAlert
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dayAlert = new System.Windows.Forms.CheckedListBox();
+            this.txtMusic = new System.Windows.Forms.TextBox();
+            this.btnMusic = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmeAlert = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chkAutoStart = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // dayAlert
+            // 
+            this.dayAlert.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dayAlert.FormattingEnabled = true;
+            this.dayAlert.Items.AddRange(new object[] {
+            "Даваа",
+            "Мягмар",
+            "Лхагва",
+            "Пүрэв",
+            "Баасан",
+            "Бямба",
+            "Ням"});
+            this.dayAlert.Location = new System.Drawing.Point(0, 0);
+            this.dayAlert.Name = "dayAlert";
+            this.dayAlert.Size = new System.Drawing.Size(120, 189);
+            this.dayAlert.TabIndex = 0;
+            this.dayAlert.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.dayAlert_ItemCheck);
+            // 
+            // txtMusic
+            // 
+            this.txtMusic.Location = new System.Drawing.Point(129, 74);
+            this.txtMusic.Name = "txtMusic";
+            this.txtMusic.ReadOnly = true;
+            this.txtMusic.Size = new System.Drawing.Size(195, 20);
+            this.txtMusic.TabIndex = 1;
+            this.txtMusic.TextChanged += new System.EventHandler(this.txtMusic_TextChanged);
+            // 
+            // btnMusic
+            // 
+            this.btnMusic.Location = new System.Drawing.Point(129, 100);
+            this.btnMusic.Name = "btnMusic";
+            this.btnMusic.Size = new System.Drawing.Size(195, 23);
+            this.btnMusic.TabIndex = 2;
+            this.btnMusic.Text = "Дууны файл сонгох";
+            this.btnMusic.UseVisualStyleBackColor = true;
+            this.btnMusic.Click += new System.EventHandler(this.btnMusic_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Тоглуулах дууны замаа сонгоорой!";
+            // 
+            // tmeAlert
+            // 
+            this.tmeAlert.CustomFormat = "HH цаг mm минут";
+            this.tmeAlert.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.tmeAlert.Location = new System.Drawing.Point(129, 25);
+            this.tmeAlert.Name = "tmeAlert";
+            this.tmeAlert.Size = new System.Drawing.Size(195, 20);
+            this.tmeAlert.TabIndex = 5;
+            this.tmeAlert.ValueChanged += new System.EventHandler(this.tmeAlert_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сэрэх цагаа оруулаарай!";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(129, 152);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(195, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Эхлүүлэх";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // chkAutoStart
+            // 
+            this.chkAutoStart.AutoSize = true;
+            this.chkAutoStart.Location = new System.Drawing.Point(129, 129);
+            this.chkAutoStart.Name = "chkAutoStart";
+            this.chkAutoStart.Size = new System.Drawing.Size(152, 17);
+            this.chkAutoStart.TabIndex = 6;
+            this.chkAutoStart.Text = "Автоматаар ажиллах уу?";
+            this.chkAutoStart.UseVisualStyleBackColor = true;
+            this.chkAutoStart.CheckedChanged += new System.EventHandler(this.chkAutoStart_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 189);
+            this.Controls.Add(this.chkAutoStart);
+            this.Controls.Add(this.tmeAlert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnMusic);
+            this.Controls.Add(this.txtMusic);
+            this.Controls.Add(this.dayAlert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Alert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox dayAlert;
+        private System.Windows.Forms.TextBox txtMusic;
+        private System.Windows.Forms.Button btnMusic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker tmeAlert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox chkAutoStart;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MorningAlert
+{
+    public partial class frmMain : Form
+    {
+        Timer _Time;
+        XmlDocument _Doc;
+        string _DocFile;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _Time = new Timer();
+            _Time.Interval = 1000;
+            _Time.Tick += _Time_Tick;
+
+            _Doc = new XmlDocument();
+            _DocFile = Application.StartupPath + "\\Alert-Setting.xml";
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            try
+            {
+                _Doc.Load(_DocFile);
+            }
+            catch { }
+            if (_Doc.DocumentElement == null)
+            {
+                _Doc.AppendChild(_Doc.CreateElement("settings"));
+            }
+            else
+            {
+                XmlNodeList nodes = _Doc.GetElementsByTagName("AlertDays");
+                if (nodes.Count > 0)
+                {
+                    XmlNode elem = (XmlNode)nodes.Item(0);
+                    string[] ndxs = elem.InnerText.Split(new char[] { ',' });
+                    foreach (string ndx in ndxs)
+                    {
+                        dayAlert.SetItemChecked(Convert.ToInt32(ndx), true);
+                    }
+                }
+                nodes = _Doc.GetElementsByTagName("AlertTime");
+                if (nodes.Count > 0)
+                {
+                    XmlNode elem = (XmlNode)nodes.Item(0);
+                    tmeAlert.Value = Convert.ToDateTime(elem.InnerText);
+                }
+                nodes = _Doc.GetElementsByTagName("AlertMusic");
+                if (nodes.Count > 0)
+                {
+                    XmlNode elem = (XmlNode)nodes.Item(0);
+                    txtMusic.Text = elem.InnerText;
+                }
+                nodes = _Doc.GetElementsByTagName("AutoStart");
+                if (nodes.Count > 0)
+                {
+                    XmlNode elem = (XmlNode)nodes.Item(0);
+                    chkAutoStart.Checked = Convert.ToBoolean(elem.InnerText);
+                }
+            }
+            if (chkAutoStart.Checked)
+            {
+                btnStart_Click(btnStart, EventArgs.Empty);
+            }
+            _Time.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _Time.Stop();
+
+            _Doc.Save(_DocFile);
+
+            base.OnClosing(e);
+        }
+
+        private void btnMusic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "(*.mp3;*.wma)|*.mp3;*.wma";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtMusic.Text = ofd.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Tag == null)
+            {
+                btnStart.Tag = "";
+            }
+            else
+            {
+                btnStart.Tag = null;
+            }
+        }
+
+        private void _Time_Tick(object sender, EventArgs e)
+        {
+            if (btnStart.Tag == null)
+            {
+                btnStart.Text = "Эхлүүлэх - " + DateTime.Now.ToString("HH:mm:ss");
+            }
+            else
+            {
+                btnStart.Text = "Зогсоох - " + DateTime.Now.ToString("HH:mm:ss");
+            }
+            bool dayChecked = false;
+            foreach (int ndx in dayAlert.CheckedIndices)
+            {
+                int checkedDow = (ndx + 1) == 7 ? 0 : (ndx + 1);
+                DayOfWeek dow = DateTime.Now.DayOfWeek;
+                if (checkedDow == (int)dow)
+                {
+                    dayChecked = true;
+                }
+            }
+            if (btnStart.Tag != null &&
+                tmeAlert.Value.ToString("HH:mm") == DateTime.Now.ToString("HH:mm") &&
+                (dayAlert.CheckedItems.Count == 0 || dayChecked))
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "wmplayer.exe";
+                psi.Arguments = "\"" + txtMusic.Text + "\"";
+                Process.Start(psi);
+                this.Close();
+            }
+        }
+
+        private void tmeAlert_ValueChanged(object sender, EventArgs e)
+        {
+            XmlNode elem = null;
+            XmlNodeList nodes = _Doc.GetElementsByTagName("AlertTime");
+            if (nodes.Count > 0)
+                elem = (XmlNode)nodes.Item(0);
+            else
+                elem = _Doc.CreateElement("AlertTime");
+            elem.InnerText = tmeAlert.Value.ToString();
+            if (nodes.Count == 0)
+            {
+                _Doc.DocumentElement.AppendChild(elem);
+            }
+            _Doc.Save(_DocFile);
+        }
+
+        private void txtMusic_TextChanged(object sender, EventArgs e)
+        {
+            XmlNode elem = null;
+            XmlNodeList nodes = _Doc.GetElementsByTagName("AlertMusic");
+            if (nodes.Count > 0)
+                elem = (XmlNode)nodes.Item(0);
+            else
+                elem = _Doc.CreateElement("AlertMusic");
+            elem.InnerText = txtMusic.Text;
+            if (nodes.Count == 0)
+            {
+                _Doc.DocumentElement.AppendChild(elem);
+            }
+            _Doc.Save(_DocFile);
+        }
+
+        private void chkAutoStart_CheckedChanged(object sender, EventArgs e)
+        {
+            XmlNode elem = null;
+            XmlNodeList nodes = _Doc.GetElementsByTagName("AutoStart");
+            if (nodes.Count > 0)
+                elem = (XmlNode)nodes.Item(0);
+            else
+                elem = _Doc.CreateElement("AutoStart");
+            elem.InnerText = chkAutoStart.Enabled.ToString();
+            if (nodes.Count == 0)
+            {
+                _Doc.DocumentElement.AppendChild(elem);
+            }
+            _Doc.Save(_DocFile);
+        }
+
+        private void dayAlert_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            XmlNode elem = null;
+            XmlNodeList nodes = _Doc.GetElementsByTagName("AlertDays");
+            if (nodes.Count > 0)
+                elem = (XmlNode)nodes.Item(0);
+            else
+                elem = _Doc.CreateElement("AlertDays");
+            List<string> lst = new List<string>();
+            foreach (int ndx in dayAlert.CheckedIndices)
+            {
+                lst.Add(ndx.ToString());
+            }
+            if (e.NewValue == CheckState.Checked)
+            {
+                lst.Add(e.Index.ToString());
+            }
+            else
+            {
+                lst.Add(e.Index.ToString());
+            }
+            elem.InnerText = string.Join(",", lst.ToArray());
+            if (nodes.Count == 0)
+            {
+                _Doc.DocumentElement.AppendChild(elem);
+            }
+            _Doc.Save(_DocFile);
+        }
+    }
+}
